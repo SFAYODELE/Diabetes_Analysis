ec980ed00ce8bcd4a406bef805b1bb7f36713647
SFAYODELE <Sfayodele@gmail.com>
2025-06-16 13:09:43 +0100

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -8,7 +8,16 @@ st.title("DIABETES ANALYSIS")
 #import my csv file
 df = pd.read_csv("diabetes_pyton.csv")
 
+
 st.markdown("## Overview")
+
+st.markdown("### First 5 observations")
+st.write(df.head())
+
+st.markdown("### Last 5 observations")
+st.write(df.tail())
+
+st.markdown("### Description of numeric data types")
 st.write(df.describe())
 
 st.markdown("## Data shape")
