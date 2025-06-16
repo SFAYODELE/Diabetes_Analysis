5c975a3438958d2f6ba5cd440f9c53b59febca1b
SFAYODELE <Sfayodele@gmail.com>
2025-06-16 14:06:58 +0100

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -33,6 +33,6 @@ st.markdown("### Blood Pressure")
 st.write(df["BloodPressure"].describe())
 
 
-fig = px.bar(df['BloodPressure'], y ='BloodPressure',title="Distribution of Blood Pressure")
+bp = px.bar(df['BloodPressure'], y ='BloodPressure',title="Distribution of Blood Pressure")
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(bp, use_container_width=True)
